0fe003baca4b2a3cf40e3bf3c2c8f71fc82b1703
Juan Fox <foxj0875@gmail.com>
2018-07-02 16:24:17 -0400

    sequelize is synced to mysql scoringproject database in server.js

--- models/index.js
+++ models/index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const confit = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 if (config.use_env_variable) {
-  const sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  const sequelize = new Sequelize(process.env[config.database, config.username, config.password, config]);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -18,7 +18,7 @@ fs
     return (file.indexOf('.') !==0 && (file !== basename) && (file.slice(-3) === '.js'));
   })
   .forEach(file => {
-    let model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Require db models
+const db = require('./models');
+
 // Test route
 app.get('/', (req, res) => {
   res.send('App is running');
 });
 
 
-// Start server to begin listening
-app.listen(PORT, () => {
-  console.log(`App listening on port: ${PORT}`);
-});
+db.sequelize.sync({force:true}).then(() => {
+  // Start server to begin listening
+  app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+  });
+})
