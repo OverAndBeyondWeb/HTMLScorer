b08f08b995a87de71e34eab52a4aedd4972b8afa
Juan Fox <foxj0875@gmail.com>
2018-07-03 16:06:38 -0400

    apiroutes get method is returning all files from the database at /api/files, post method is creating 1 file database entry with id, name, createdAt, and updatedAt

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,8 +6,22 @@ const router = require('express').Router();
 
 module.exports = app => {
 
+  // Get all files from the database
   router.get('/api/files', (req, res) => {
-    res.send('These are the files');
+    db.File.findAll({})
+      .then(files => {
+        res.json(files);
+      })
+      .catch();
+  });
+
+  // Create 1 file from name off of request body
+  router.post('/api/file', (req, res) => {
+    db.File.create({name: req.body.name})
+      .then(file => {
+        res.json(file);
+      })
+      .catch();
   });
 
   return router;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Express app setup
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Work with request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Require db models
 const db = require('./models');
