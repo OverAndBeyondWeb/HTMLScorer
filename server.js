1f3fcf687eccab89e11e73ab6ac284aa44905d99
Juan Fox <foxj0875@gmail.com>
2019-02-17 17:06:43 -0500

    server.js is setup to serve index.js from client/build

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ const db = require('./models');
 app.use(require('./routes/apiRoutes')(app));
 //require('./seed')(app);
 
+if (process.env.NODE_ENV) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 db.sequelize.sync({}).then(() => {
   // Start server to begin listening
   app.listen(PORT, () => {
