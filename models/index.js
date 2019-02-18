11d5bd53a4048e16568dd055ce5c22096025e1dd
Juan Fox <foxj0875@gmail.com>
2019-02-18 14:30:33 -0500

    test

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
-
+console.log(config);
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
